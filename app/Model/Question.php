b310838afa727382fc45b3ddde01ec567939508a
Mridul <mridulfeni94@gmail.com>
2019-11-29 04:41:04 -0800

    vue blog initial stage and question api maked

---
+++ app/Model/Question.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Model;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+//    protected $fillable = ['title','slug','body','category_id','user_id'];
+
+      protected $guarded =[];
+
+
+//      get  question by slug without use id
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
+    }
+
+
+
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+}
